746063054f23a46d4d169174b5c8d53cb4deb5bb
nayu7u <19748682+nayu7u@users.noreply.github.com>
2023-03-26 20:32:18 +0900

    feat: 回数指定できるように

--- for/repeat.go
+++ for/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, n int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < n; i++ {
 		repeated += character
 	}
 	return repeated
